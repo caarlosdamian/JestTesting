9f75d51ec8170872f506c2b65ff62fe69423b19b
caarlosdamian <dapecarlos@gmail.com>
2021-11-02 12:50:28 -0600

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Counter Testing", () => {
+  test("renders the title of counter", () => {
+   const {getByText} = render(<App />);
+    const linkElement = getByText("This is counter app");
+    expect(linkElement).toBeInTheDocument();
+  });
 });
